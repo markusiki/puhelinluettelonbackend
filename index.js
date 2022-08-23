b8972c8da341ff3bac34aa28e4efd515213831eb
Markus Kitunen <markus.kitunen@outlook.com>
2022-08-23 17:23:25 +0300

    index.js updated, Procfile added

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const PORT = 3001
-app.listen(PORT)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
+})
